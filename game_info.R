faca3ecaf0c03f42fc12af7edf99598ada39a62d
Eric Feder <efeder@theladders.com>
2014-07-22 17:59:06 -0400

    more work on extracting game attributes

--- game_info.R
+++ game_info.R
@@ -1,5 +1,15 @@
-episodes <- readHTMLTable("http://www.j-archive.com/showseason.php?season=30", stringsAsFactors=F, header=F)[[1]]
-id.date.split <- strsplit(episodes[, 1], ", aired..", fixed=F)
+# Load package
+library(XML)
+
+# Download list of episodes
+episodes.raw <- readHTMLTable("http://www.j-archive.com/showseason.php?season=30", stringsAsFactors=F, header=F)[[1]]
+
+# Split into components
+id.date.split <- strsplit(episodes.raw[, 1], ", aired.", fixed=F)
 episode.id <- as.numeric(gsub("#", "", sapply(id.date.split, function(x) x[1])))
 date <- as.Date(sapply(id.date.split, function(x) x[2]))
-contestants <- strsplit(episodes[, 2], " vs. ")
+contestants <- strsplit(episodes.raw[, 2], " vs. ")
+contestants.list <- lapply(1:3, function(x) sapply(contestants, function(y) y[x]))
+
+# Save as single data frame
+episodes <- data.frame(episode.id, date, left.contestant=contestants.list[[1]], center.contestant=contestants.list[[2]], right.contestant=contestants.list[[3]], notes=episodes.raw[, 3])
