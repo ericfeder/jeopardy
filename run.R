989e12251836bc34fb65a6f7807b934715d85381
Eric Feder <efeder@theladders.com>
2014-07-23 16:41:41 -0400

    download all seasons and all doubled non-tourny games

--- run.R
+++ run.R
@@ -2,8 +2,17 @@
 library(data.table)
 library(rpart)
 
+# Download seasons
+season.nums <- 1:30
+game.info <- lapply(season.nums, downloadSeason)
+game.info.rbind <- rbindlist(game.info)
+
+# Add game info
+all.game.info <- game.info.rbind[order(date), ]
+all.game.info <- addInfo(all.game.info)
+
 # Download games
-ids <- 4450:4459
+ids <- all.game.info[values.doubled & !tournament, j.archive.id]
 games.raw <- lapply(ids, downloadGame)
 
 # Analyze games
