223ce17880f7c88259bd986664e36bc499b9accd
Eric Feder <efeder@theladders.com>
2014-07-24 13:48:04 -0400

    reorganize data_preparation and download all game

--- data_preparation.R
+++ data_preparation.R
@@ -2,16 +2,18 @@
 library(data.table)
 
 # Download seasons
+source("game_info.R")
 season.nums <- 1:30
 game.info <- lapply(season.nums, downloadSeason)
 game.info.rbind <- rbindlist(game.info)
+game.info.rbind <- game.info.rbind[order(date), ]
 
 # Add game info
-all.game.info <- game.info.rbind[order(date), ]
-all.game.info <- addInfo(all.game.info)
+all.game.info <- addInfo(game.info.rbind)
 
 # Download games
-ids <- all.game.info[values.doubled & !tournament, j.archive.id]
+source("download_games.r")
+ids <- all.game.info$j.archive.id
 games.raw <- lapply(ids, downloadGame)
 
 # Analyze games
