819fb567d8ddadb0a6f0c7d7caa42acfbd933dc0
Eric Feder <efeder@theladders.com>
2014-07-25 17:15:27 -0400

    better subsetting for random forest

--- fit_random_forest.R
+++ fit_random_forest.R
@@ -1,10 +1,14 @@
 # Load packages
-library(rpart)
+library(randomForest)
+library(data.table)
+
+# Subset data for model
+usable.j.archive.ids <- all.game.info[values.doubled & !tournament, j.archive.id]
+usable.points <- games.rbind[round != "FinalJeopardy" & j.archive.id %in% usable.j.archive.ids]
 
 # Fit random forest
-library(randomForest)
-rf <- randomForest(factor(winner.rank) ~ middle.diff + middle.ratio + bottom.diff + bottom.ratio + money.left + dd.remaining, data=games.rbind, subset=round != "FinalJeopardy", do.trace=2, ntree=100, na.action=na.omit)
+rf <- randomForest(factor(winner.rank) ~ middle.diff + middle.ratio + bottom.diff + bottom.ratio + money.left + dd.remaining, data=usable.points, do.trace=2, ntree=100)
 
 # Predict
-preds <- predict(rf, subset(games.rbind, round != "FinalJeopardy"), type="prob")
-games.odds <- data.frame(subset(games.rbind, round != "FinalJeopardy"), preds)
+preds <- predict(rf, usable.points, type="prob")
+games.odds <- data.frame(usable.points, preds)
