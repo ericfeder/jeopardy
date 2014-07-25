84d35357e0bf9da144c594c57a8d28e95d734427
Eric Feder <efeder@theladders.com>
2014-07-25 13:51:19 -0400

    add returning champ info to game.info.rbing and create game.results

--- data_preparation.R
+++ data_preparation.R
@@ -8,9 +8,6 @@ game.info <- lapply(season.nums, downloadSeason)
 game.info.rbind <- rbindlist(game.info)
 game.info.rbind <- game.info.rbind[order(date), ]
 
-# Add game info
-all.game.info <- addInfo(game.info.rbind)
-
 # Download games
 source("download_games.r")
 games.raw <- lapply(all.game.info$j.archive.id, downloadGame)
@@ -19,6 +16,13 @@ games.raw <- lapply(all.game.info$j.archive.id, downloadGame)
 source("process_games.R")
 games <- lapply(games.raw, processGame)
 games.rbind <- rbindlist(games[sapply(games, is.data.frame)])
+setnames(games.rbind, 2:4, c("left", "center", "right"))
 
-# Check if returning champion
+# Add game info
+all.game.info <- addInfo(game.info.rbind)
 returning.champion <- checkReturningChampion(sapply(games.raw, function(x) x$scores.raw$contestants_table$V2))
+all.game.info$num.champs <- returning.champion$num.champs
+all.game.info$champ.days <- returning.champion$days
+
+# Extract game results
+game.results <- games.rbind[round == "FinalJeopardy", list(j.archive.id, left, center, right, left.rank, center.rank, right.rank)]
