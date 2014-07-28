8d57609fdcab7cf5d357ac7df377c67fa0f47a47
Eric Feder <efeder@theladders.com>
2014-07-28 17:46:48 -0400

    try new knn model

--- Scripts/Models/fit_knn.R
+++ Scripts/Models/fit_knn.R
@@ -1,15 +1,15 @@
 # Load packages
 library(data.table)
-library(FNN)
+library(kknn)
 
 # Fit k-NN
-model <- knn.cv(scale(usable.points[, list(middle.diff, middle.ratio, bottom.diff, bottom.ratio, money.left, dd.remaining)]),
-                usable.points[, factor(winner.rank)],
+knnOneGame <- function(id){
+  model <- kknn(factor(winner.rank) ~ middle.diff + middle.ratio +  bottom.diff + bottom.ratio + money.left + dd.remaining,
+                test=usable.points[j.archive.id == id],
+                train=usable.points[j.archive.id != id],
                 k=200,
-                prob=T)
-game.odds <- data.table(usable.points, pred.winner=model, odds=attr(model, "prob"))
-
-# Evaluate
-rounded <- round(game.odds$odds[game.odds$pred.winner == 1] / 0.05) * 0.05
-plot(sort(unique(rounded)), tapply(game.odds$winner.rank[game.odds$pred.winner == 1], rounded / 100, FUN=function(x) table(x)[1]/sum(table(x))))
-abline(a=0, b=1)
+                kernel="triangular")
+  cat("kknn for game", id, "\n")
+  return(model$prob)
+}
+kknn.model <- lapply(unique(usable.points$j.archive.id), knnOneGame)
