674cc063e9ba56d97299bbbc6572e790bcd73519
Eric Feder <efeder@theladders.com>
2014-07-28 18:11:30 -0400

    random forest works better with smaller sampsize?

--- Scripts/Models/fit_random_forest.R
+++ Scripts/Models/fit_random_forest.R
@@ -3,10 +3,10 @@ library(data.table)
 library(randomForest)
 
 # Fit random forest
-rf.model <- randomForest(factor(winner.rank) ~ middle.diff + middle.ratio + bottom.diff + bottom.ratio + money.left + dd.remaining, data=usable.points, do.trace=2, ntree=100)
+rf.model <- randomForest(factor(winner.rank) ~ middle.diff + middle.ratio + bottom.diff + bottom.ratio + money.left + dd.remaining, data=usable.points, do.trace=2, ntree=100, sampsize=1000)
 
 # Predict
-rf.model <- list(model=rf.model, preds=predict(rf.model, usable.points, type="prob"))
+rf.model <- list(model=rf.model, preds=predict(rf.model, type="prob"))
 
 # Evaluate calibration
 source("Scripts/Models/evaluate_calibration.R")
