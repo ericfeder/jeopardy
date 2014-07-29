6fe0a7cf9759fcb3208f2e70eba85273975f7dc1
Eric Feder <efeder@theladders.com>
2014-07-29 18:04:34 -0400

    tinker and test gbm

--- Scripts/Models/fit_gbm.R
+++ Scripts/Models/fit_gbm.R
@@ -6,14 +6,15 @@ library(gbm)
 test.ids <- sample(unique(usable.points$j.archive.id), 500)
 
 # Fit model
-gbm.model <- gbm(factor(winner.rank) ~ middle.diff + middle.ratio + bottom.diff + bottom.ratio + money.left + dd.remaining, data=usable.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.2, n.trees=150, verbose=T)
+gbm.model <- gbm(factor(winner.rank) ~ middle.diff + middle.ratio + bottom.diff + bottom.ratio + money.left + dd.remaining, data=usable.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.2, n.trees=150, verbose=T, n.minobsinnode=50)
 
 # Predict
-preds <- predict(gbm.model, usable.points[j.archive.id %in% test.ids], n.trees=50, type="response")[, , 1]
-gbm.model <- list(model=gbm.model, preds=predict(gbm.model, usable.points, n.trees=80, type="response")[, , 1])
+preds <- predict(gbm.model, usable.points[j.archive.id %in% test.ids], n.trees=100, type="response")[, , 1]
+# gbm.model <- list(model=gbm.model, preds=predict(gbm.model, usable.points, n.trees=80, type="response")[, , 1])
 
 # Evaluate calibration
 source("Scripts/Models/evaluate_calibration.R")
 evaluateCalibration(preds[, 1], usable.points[j.archive.id %in% test.ids, winner.rank] == 1, units=0.05)
-abline(h=1/3, lty=3)
-abline(v=1/3, lty=3)
+hist(preds[, 1])
+range(preds[, 1])
+View(data.table(preds, usable.points[j.archive.id %in% test.ids, list(top.score, middle.score, bottom.score, money.left, dd.remaining, j.archive.id)])[j.archive.id == 4546])
