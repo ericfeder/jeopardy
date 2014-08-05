b23fff4a4ca461b24f84964e9bc36866ae1ce98c
Eric Feder <efeder@theladders.com>
2014-08-05 18:37:43 -0400

    separate script for cross validation

--- Scripts/Models/fit_gbm.R
+++ Scripts/Models/fit_gbm.R
@@ -2,24 +2,12 @@
 library(data.table)
 library(gbm)
 
-# Select random games
-test.ids <- sample(unique(usable.points$j.archive.id), 500)
-
 # Fit model
-gbm.model <- gbm(factor(winner.rank) ~ middle.diff + middle.ratio + bottom.diff + bottom.ratio + money.left + dd.remaining, data=usable.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=800, verbose=T)
+gbm.model <- gbm(factor(winner.rank) ~ middle.diff + middle.ratio + bottom.diff + bottom.ratio + money.left + dd.remaining, data=usable.points, distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T)
 
 # Predict
-preds <- predict(gbm.model, usable.points[j.archive.id %in% test.ids], n.trees=500, type="response")[, , 1]
-# gbm.model <- list(model=gbm.model, preds=predict(gbm.model, usable.points, n.trees=80, type="response")[, , 1])
+gbm.model <- list(model=gbm.model, preds=predict(gbm.model, usable.points, n.trees=700, type="response")[, , 1])
 
 # Evaluate calibration
 source("Scripts/Models/evaluate_calibration.R")
-with(usable.points[j.archive.id %in% test.ids], evaluateModel(data.frame(top.score, middle.score, bottom.score), preds, winner.rank, j.archive.id, units=0.05))
-
-# Check sample game
-rand.game <- data.table(preds, usable.points[j.archive.id %in% test.ids, list(top.score, middle.score, bottom.score, money.left, dd.remaining, j.archive.id)])[j.archive.id == sample(test.ids, 1)]
-
-rand.game$prob1.change <- round(c(NA, diff(rand.game$"1")), 2)
-rand.game$score1.change <- c(NA, diff(rand.game$top.score))
-rand.game$score2.change <- c(NA, diff(rand.game$middle.score))
-rand.game$score3.change <- c(NA, diff(rand.game$bottom.score))
+with(usable.points, evaluateModel(data.frame(top.score, middle.score, bottom.score), gbm.model$preds, winner.rank, j.archive.id, units=0.05))
