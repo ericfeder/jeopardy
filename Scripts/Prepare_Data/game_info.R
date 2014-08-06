1d7315afebbb55572b0796cfaad295ec9e639790
Eric Feder <efeder@theladders.com>
2014-08-06 12:13:35 -0400

    split addInfo into separate file

--- Scripts/Prepare_Data/game_info.R
+++ Scripts/Prepare_Data/game_info.R
@@ -30,10 +30,3 @@ downloadSeason <- function(num){
   # Return
   return(episodes)
 }
-
-# Function to extract additional game information
-addInfo <- function(info){
-  info$values.doubled <- info$date >= as.Date("2001-11-26")
-  info$tournament <- grepl("Battle of the Decades|Tournament|Championship|Kids Week|Power Players Week|The IBM Challenge|Celebrity|Million Dollar|Back to School Week", info$notes, ignore.case=T) & !grepl("announces|drawing", info$notes, ignore.case=T)
-  return(info)
-}
