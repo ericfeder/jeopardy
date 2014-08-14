774f4a54bf223b967f60f1135eaf58415c8e70be
Eric Feder <efeder@theladders.com>
2014-08-14 17:20:48 -0400

    add steps to prepare.R

--- shiny/prepare.R
+++ shiny/prepare.R
@@ -1,2 +1,12 @@
+# Load packages
+library(dplyr)
+
+# Load workspaces
+load("Workspaces/prepared_data.RData")
+load("Workspaces/gbm_model.RData")
+
+# Load function to visualize data
+source("Scripts/Visualize/rchart_visualize.R")
+
 # Prepare rows for dropdowns
 game.info <- all.game.info %>% mutate(game.strings=sprintf("%s vs. %s vs. %s (%s)", left.contestant, center.contestant, right.contestant, date)) %>% filter(tournament == F) %>% select(season, j.archive.id, game.strings)
