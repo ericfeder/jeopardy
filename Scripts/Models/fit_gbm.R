702aa96e11c1c90a34d90a07c2f4d4ce165d1f1b
Eric Feder <efeder@theladders.com>
2014-08-18 17:39:29 -0400

    include days for reigning champion in gbm model

--- Scripts/Models/fit_gbm.R
+++ Scripts/Models/fit_gbm.R
@@ -3,7 +3,8 @@ library(data.table)
 library(gbm)
 
 # Fit model
-gbm.model <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining, data=modeling.points, distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T)
+gbm.model <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days, data=modeling.points, distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T)
+# gbm.model <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + left.rank + champ.days, data=modeling.points, distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T)
 
 # Predict
 gbm.model <- list(model=gbm.model, preds=predict(gbm.model, modeling.points, n.trees=700, type="response")[, , 1])
