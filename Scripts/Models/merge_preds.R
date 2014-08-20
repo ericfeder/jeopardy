52284abe065e6b015658d6a53c9f5e824c29ee8b
Eric Feder <ef2240@columbia.edu>
2014-08-19 21:14:46 -0400

    include round and question number when merging preds with data

--- Scripts/Models/merge_preds.R
+++ Scripts/Models/merge_preds.R
@@ -3,11 +3,11 @@ library(data.table)
 
 # Function to merge odds with data
 mergePredsWithData <- function(preds, points){
-  merged <- data.table(preds, points[, list(left.rank, center.rank, right.rank, left, center, right, money.left, dd.remaining, j.archive.id)])
-  merged$left.prob <- data.frame(merged)[cbind(1:nrow(merged), merged$left.rank)] * 100
-  merged$center.prob <- data.frame(merged)[cbind(1:nrow(merged), merged$center.rank)] * 100
-  merged$right.prob <- data.frame(merged)[cbind(1:nrow(merged), merged$right.rank)] * 100
-  merged.select <- merged[, list(left, center, right, left.prob, center.prob, right.prob)]
+  merged <- data.table(preds, points)
+  merged$left.prob <- data.frame(preds)[cbind(1:nrow(merged), merged$left.rank)] * 100
+  merged$center.prob <- data.frame(preds)[cbind(1:nrow(merged), merged$center.rank)] * 100
+  merged$right.prob <- data.frame(preds)[cbind(1:nrow(merged), merged$right.rank)] * 100
+  merged.select <- merged[, list(left, center, right, left.prob, center.prob, right.prob, round, num.q)]
 
   merged.split <- split(merged.select, merged$j.archive.id)
   return(merged.split)
