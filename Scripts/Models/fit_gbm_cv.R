5feaf8d97e1d9f918cb59806a8051afb2569cd4b
Eric Feder <efeder@theladders.com>
2014-08-21 16:32:02 -0400

    compare 2 models in fit_gbm_cv

--- Scripts/Models/fit_gbm_cv.R
+++ Scripts/Models/fit_gbm_cv.R
@@ -6,11 +6,14 @@ library(gbm)
 test.ids <- sample(unique(modeling.points$j.archive.id), 850)
 
 # Fit model
-gbm.model <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T)
+gbm.model <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T, interaction.depth=2)
+gbm.model.new <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T, interaction.depth=1, n.minobsinnode=4)
 
 # Predict
 preds <- predict(gbm.model, modeling.points[j.archive.id %in% test.ids], n.trees=1000, type="response")[, , 1]
+preds.new <- predict(gbm.model.new, modeling.points[j.archive.id %in% test.ids], n.trees=1000, type="response")[, , 1]
 
 # Evaluate calibration
 source("Scripts/Models/evaluate_calibration.R")
 with(modeling.points[j.archive.id %in% test.ids], evaluateModel(data.frame(top.score, middle.score, bottom.score), preds, winner.rank, j.archive.id, units=0.05))
+with(modeling.points[j.archive.id %in% test.ids], evaluateModel(data.frame(top.score, middle.score, bottom.score), preds.new, winner.rank, j.archive.id, units=0.05))
