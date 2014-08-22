b79819a5bbfa965ae8589654662aaa47b2796398
Eric Feder <efeder@theladders.com>
2014-08-22 17:05:42 -0400

    source set_bounds in process_games

--- Scripts/Prepare_Data/process_games.R
+++ Scripts/Prepare_Data/process_games.R
@@ -98,12 +98,7 @@ returnValues <- function(html, doubled){
 }
 
 # Function to set both min and max of vector
-setBounds <- function(vec, min, max, na.to.1){
-  if (na.to.1) vec[is.na(vec)] <- 1
-  nothing.below.min <- pmax(0, vec)
-  nothing.above.max <- pmin(max, nothing.below.min)
-  return(nothing.above.max)
-}
+source("Scripts/Prepare_Data/set_bounds.R")
 
 # Function to add variables to game
 addVariables <- function(game, doubled, champ.days){
