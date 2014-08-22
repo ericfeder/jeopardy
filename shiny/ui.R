1cfa389e93fae64f5fad12db4036209b24fdcd34
Eric Feder <ef2240@columbia.edu>
2014-08-21 22:00:33 -0400

    add navbar to UI, fix spacing of ui.R

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,23 +1,26 @@
 # Define UI
-shinyUI(fluidPage(
+shinyUI(
+  navbarPage("Jeopardy Win Probability",
+             tabPanel("Games",
+                      sidebarLayout(
+                        # Input Panel
+                        sidebarPanel(
+                          selectInput("season", label="Season: ", choices=seasons$season.string, selected=seasons$season.string[30]),
+                          selectInput("game.description", label="Game: ", choices=game.info[season == 30, game.strings], selected=game.info[season == 30, game.strings][1]),
+                          selectInput("var", choices=c("Odds", "Score"), label="Metric"),
+                          width=4
+                          ),
 
-  #  Application title
-  titlePanel("Jeopardy Win Probability"),
+                        # Show data
+                        mainPanel(
+                          uiOutput("game_info"),
+                          showOutput("viz", lib="morris")
+                          )
+                        )
+                      ),
 
-
-  sidebarLayout(
-    # Input Panel
-    sidebarPanel(
-      selectInput("season", label="Season: ", choices=seasons$season.string, selected=seasons$season.string[30]),
-      selectInput("game.description", label="Game: ", choices=game.info[season == 30, game.strings], selected=game.info[season == 30, game.strings][1]),
-      selectInput("var", choices=c("Odds", "Score"), label="Metric"),
-      width=4
-    ),
-    # Show data
-    mainPanel(
-      uiOutput("game_info"),
-      showOutput("viz", lib="morris")
-      )
-    )
+             tabPanel("Test Game State",
+                      numericInput("left.score", label="Left Contestant Score:", value=0)
+                      )
+             )
   )
-)
