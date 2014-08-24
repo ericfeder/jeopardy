0d4bd8cbb8a9c7123247234e18a58905acfa619d
Eric Feder <ef2240@columbia.edu>
2014-08-24 00:47:29 -0400

    rename rchart_visualize to visualize_game

--- shiny/prepare.R
+++ shiny/prepare.R
@@ -3,7 +3,7 @@ load("Workspaces/prepared_data.RData")
 load("Workspaces/gbm_model.RData")
 
 # Load functions to visualize data
-source("Scripts/Visualize/rchart_visualize.R")
+source("Scripts/Visualize/visualize_game.R")
 source("Scripts/Visualize/visualize_odds.R")
 
 # Prepare game info for shiny
