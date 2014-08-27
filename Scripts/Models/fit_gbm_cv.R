afb7db16d4d4dd4c968e00bce41a57e2ab5eef37
Eric Feder <efeder@theladders.com>
2014-08-27 11:36:42 -0400

    test adding perc.lock and check outliers

--- Scripts/Models/fit_gbm_cv.R
+++ Scripts/Models/fit_gbm_cv.R
@@ -6,8 +6,8 @@ library(gbm)
 test.ids <- sample(unique(modeling.points$j.archive.id), 850)
 
 # Fit model
-gbm.model <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T, interaction.depth=2)
-gbm.model.new <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T, interaction.depth=1, n.minobsinnode=4)
+gbm.model <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=2000, verbose=T, interaction.depth=2)
+gbm.model.new <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days + money.til.lock, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=2000, verbose=T, interaction.depth=2)
 
 # Predict
 preds <- predict(gbm.model, modeling.points[j.archive.id %in% test.ids], n.trees=1000, type="response")[, , 1]
@@ -17,3 +17,15 @@ preds.new <- predict(gbm.model.new, modeling.points[j.archive.id %in% test.ids],
 source("Scripts/Models/evaluate_calibration.R")
 evaluateModel(modeling.points[j.archive.id %in% test.ids], preds, units=0.05)
 evaluateModel(modeling.points[j.archive.id %in% test.ids], preds.new, units=0.05)
+
+# Show largest positive example
+row <- which.max(preds[, 1] - preds.new[, 1])
+modeling.points[j.archive.id %in% test.ids][row, ]
+preds[row, ]
+preds.new[row, ]
+
+# Show largest negative example
+row <- which.min(preds[, 1] - preds.new[, 1])
+modeling.points[j.archive.id %in% test.ids][row, ]
+preds[row, ]
+preds.new[row, ]
