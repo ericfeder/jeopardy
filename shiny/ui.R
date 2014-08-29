dff2ca90d9027f6e161dc19013ac65b9ec3a34dd
Eric Feder <efeder@theladders.com>
2014-08-29 11:12:46 -0400

    dropdowns shouldn't get cut off on bottom

--- shiny/ui.R
+++ shiny/ui.R
@@ -15,25 +15,28 @@ shinyUI(
                         mainPanel(
                           uiOutput("game_info"),
                           showOutput("viz", lib="morris")
-                        )
+                        ),
+                        fluid=F
                       )
              ),
 
              tabPanel("Test Game State",
+                      showOutput("oddsviz", lib="nvd3"),
+                      hr(),
                       fluidRow(
-                        showOutput("oddsviz", lib="nvd3"),
-                        hr(),
                         column(3,
                                numericInput("left.score", label="Left Contestant Score:", value=0),
                                numericInput("center.score", label="Center Contestant Score:", value=0),
                                numericInput("right.score", label="Right Contestant Score:", value=0)),
                         column(3,
                                sliderInput("money.left", label="Money Remaining on Board", min=0, max=54000, value=54000, step=200, format="$#,##0"),
-                               selectInput("dd.remaining", label="Daily Doubles Remaining", choices=0:3, selected=3)),
+                               selectInput("dd.remaining", label="Daily Doubles Remaining", choices=0:3, selected=3)
+                               ),
                         column(3,
                                selectInput("champ.days", label="How many games has the left contestant won before today?", choices=c(0:3, "4+"), selected=0)
                         )
-                      )
+                      ),
+                      hr()
              ),
 
              tabPanel("About",
