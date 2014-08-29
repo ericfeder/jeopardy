ae047da60346abaea9a63d24c840498a01d747e6
Eric Feder <efeder@theladders.com>
2014-08-29 12:25:16 -0400

    move shiny preparation scripts into separate folder

---
+++ Scripts/For_Shiny/prepare.R
@@ -0,0 +1,13 @@
+# Load workspaces
+load("Workspaces/prepared_data.RData")
+load("Workspaces/gbm_model.RData")
+
+# Load functions to visualize data
+source("Scripts/Visualize/visualize_game.R")
+source("Scripts/Visualize/visualize_odds.R")
+
+# Prepare game info for shiny
+source("Scripts/For_Shiny/prepare_game_info.R")
+
+# Save workspace for shiny
+save(all.game.info, game.info, seasons, fitInputs, visualizeOdds, setBounds, gbm.model, game.odds.split, formatAsPercent, formatAsDollar, prepareForVisualization, hoverFunction, visualizeGame, file="shiny/shiny_workspace.RData")
