7b6821ddca2705861266e7d1985b4ad790bec3a4
Eric Feder <efeder@theladders.com>
2014-09-03 17:58:52 -0400

    bug in setBounds

--- Scripts/Prepare_Data/set_bounds.R
+++ Scripts/Prepare_Data/set_bounds.R
@@ -1,6 +1,6 @@
 # Function to set both min and max of vector
 setBounds <- function(vec, min, max, set.na=NULL){
-  nothing.below.min <- pmax(0, vec)
+  nothing.below.min <- pmax(min, vec)
   nothing.above.max <- pmin(max, nothing.below.min)
   if (!is.null(set.na)) nothing.above.max[is.na(nothing.above.max)] <- set.na
   return(nothing.above.max)
