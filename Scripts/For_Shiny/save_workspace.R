bce331bf5d4b2c9b3cd533ebe4ce686a9ecbdb08
Eric Feder <efeder@theladders.com>
2014-09-04 18:16:33 -0400

    don't need functions when saving workspace

--- Scripts/For_Shiny/save_workspace.R
+++ Scripts/For_Shiny/save_workspace.R
@@ -2,10 +2,6 @@
 load("Workspaces/prepared_data.RData")
 load("Workspaces/gbm_model.RData")
 
-# Load functions to visualize data
-source("Scripts/Visualize/visualize_game.R")
-source("Scripts/Visualize/visualize_odds.R")
-
 # Prepare game info for shiny
 source("Scripts/For_Shiny/prepare_game_info.R")
 
