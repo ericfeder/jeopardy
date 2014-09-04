cbb7a0275fde96a714b625b0a101260f2b1228e0
Eric Feder <efeder@theladders.com>
2014-09-04 16:59:32 -0400

    new attempts for fit_gbm_cv

--- Scripts/Models/fit_gbm_cv.R
+++ Scripts/Models/fit_gbm_cv.R
@@ -6,12 +6,12 @@ library(gbm)
 test.ids <- sample(unique(modeling.points$j.archive.id), 850)
 
 # Fit model
-gbm.model <- gbm(factor(winner.rank) ~ money.left.adj + middle.diff.adj + bottom.diff.adj + middle.ratio + bottom.ratio + dd.remaining + top.days + middle.days + bottom.days + perc.for.lock + perc.for.one.lock + perc.for.crush + perc.for.one.crush + perc.for.lead + perc.for.one.lead, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T, interaction.depth=2)
-gbm.model.new <- gbm(factor(winner.rank) ~ middle.ratio + bottom.ratio + dd.remaining + top.days + middle.days + bottom.days + perc.for.lock + perc.for.one.lock + perc.for.crush + perc.for.one.crush + perc.for.lead + perc.for.one.lead, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=1500, verbose=T, interaction.depth=2)
+gbm.model <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days + lock.perc + crush.perc + lead.perc, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.02, n.trees=800, verbose=T, interaction.depth=2)
+gbm.model.new <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days + lock.perc + crush.perc + lead.perc + factor(tie.for.first) + factor(tie.for.second), data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.02, n.trees=800, verbose=T, interaction.depth=2)
 
 # Predict
-preds <- predict(gbm.model, modeling.points[j.archive.id %in% test.ids], n.trees=1000, type="response")[, , 1]
-preds.new <- predict(gbm.model.new, modeling.points[j.archive.id %in% test.ids], n.trees=1000, type="response")[, , 1]
+preds <- predict(gbm.model, modeling.points[j.archive.id %in% test.ids], n.trees=500, type="response")[, , 1]
+preds.new <- predict(gbm.model.new, modeling.points[j.archive.id %in% test.ids], n.trees=500, type="response")[, , 1]
 
 # Evaluate calibration
 source("Scripts/Models/evaluate_model.R")
@@ -20,12 +20,16 @@ evaluateModel(modeling.points[j.archive.id %in% test.ids], preds.new, units=0.05
 
 # Show largest positive example
 row <- which.max(preds[, 1] - preds.new[, 1])
+# row <- which(preds[, 1] - preds.new[, 1] > 0.035)
 modeling.points[j.archive.id %in% test.ids][row, ]
 preds[row, ]
 preds.new[row, ]
 
 # Show largest negative example
 row <- which.min(preds[, 1] - preds.new[, 1])
+# row <- which(preds[, 1] - preds.new[, 1] < -0.1)
 modeling.points[j.archive.id %in% test.ids][row, ]
 preds[row, ]
 preds.new[row, ]
+
+modeling.points[money.left == 0 & middle.days >= 2 & middle.ratio > 0.8, table(winner.rank)/.N]
