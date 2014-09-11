90a7a87a22059eb5c4657b132390cdfe0aa733c3
Eric Feder <ef2240@columbia.edu>
2014-09-11 01:38:31 -0400

    more attempts to handle ties

--- scripts/build_models/handle_ties.R
+++ scripts/build_models/handle_ties.R
@@ -1,7 +1,7 @@
 # Function to properly handle tie games when fitting gbm
-points <- modeling.points[j.archive.id %in% test.ids]
+points <- copy(modeling.points)
 model <- gbm.model
-n.trees <- 700
+n.trees <- 450
 
 # Fit model once
 first.run <- predict(model, points, n.trees=n.trees, type="response")[, , 1]
@@ -28,15 +28,28 @@ points3 <- copy(points)
 points3[tie.for.first & tie.for.second, `:=`(middle.days=bottom.days, bottom.days=top.days, top.days=middle.days.temp)]
 points4 <- copy(points)
 points4[tie.for.first & tie.for.second, `:=`(middle.days=top.days, top.days=bottom.days, bottom.days=middle.days.temp)]
+points5 <- copy(points)
+points5[tie.for.first & tie.for.second, `:=`(middle.days=bottom.days, bottom.days=middle.days.temp)]
+points6 <- copy(points)
+points6[tie.for.first & tie.for.second, `:=`(middle.days=top.days, top.days=middle.days.temp)]
+points7 <- copy(points)
+points7$top.days.temp <- points7$top.days
+points7[tie.for.first & tie.for.second, `:=`(bottom.days=top.days.temp, top.days=bottom.days)]
 
 # Fit models and switch odds back
 third.run <- predict(model, points3, n.trees=n.trees, type="response")[, , 1]
 fourth.run <- predict(model, points4, n.trees=n.trees, type="response")[, , 1]
-third.run[tie.for.first & tie.for.second, 1:3] <- third.run[tie.for.first & tie.for.second, c(3, 1, 2)]
-fourth.run[tie.for.first & tie.for.second, 1:3] <- third.run[tie.for.first & tie.for.second, c(2, 3, 1)]
+fifth.run <- predict(model, points5, n.trees=n.trees, type="response")[, , 1]
+sixth.run <- predict(model, points6, n.trees=n.trees, type="response")[, , 1]
+seventh.run <- predict(model, points7, n.trees=n.trees, type="response")[, , 1]
+third.run[tie.for.first & tie.for.second, ] <- third.run[tie.for.first & tie.for.second, c(3, 1, 2)]
+fourth.run[tie.for.first & tie.for.second, ] <- fourth.run[tie.for.first & tie.for.second, c(2, 3, 1)]
+fifth.run[tie.for.first & tie.for.second, c(2, 3)] <- fifth.run[tie.for.first & tie.for.second, c(3, 2)]
+sixth.run[tie.for.first & tie.for.second, c(1, 2)] <- sixth.run[tie.for.first & tie.for.second, c(2, 1)]
+seventh.run[tie.for.first & tie.for.second, c(1, 3)] <- seventh.run[tie.for.first & tie.for.second, c(3, 1)]
 
-avg <- (first.run + second.run) / 2
-difs <- avg - first.run
-
-avg3 <- (first.run + third.run + fourth.run) / 3
-difs <- avg3 - first.run
+# Average over different runs
+probs <- (first.run + second.run) / 2
+avg3 <- (first.run + third.run + fourth.run + fifth.run + sixth.run + seventh.run) / 6
+probs[tie.for.first & tie.for.second, ] <- avg3[tie.for.first & tie.for.second, ]
+colnames(probs) <- c("top.prob", "middle.prob", "bottom.prob")
