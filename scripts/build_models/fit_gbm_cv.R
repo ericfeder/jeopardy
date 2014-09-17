67db0046410f6194b2f8f6d67328fade3965cc39
Eric Feder <efeder@theladders.com>
2014-09-17 16:55:40 -0400

    test interaction.depth

--- scripts/build_models/fit_gbm_cv.R
+++ scripts/build_models/fit_gbm_cv.R
@@ -1,3 +1,6 @@
+# Load data
+load("data/workspaces/prepared_data.RData")
+
 # Load package
 library(data.table)
 library(gbm)
@@ -6,20 +9,21 @@ library(gbm)
 test.ids <- sample(unique(modeling.points$j.archive.id), 850)
 
 # Fit model
-gbm.model <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days + lock.perc + crush.perc + lead.perc, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.01, n.trees=2000, verbose=T, interaction.depth=2)
+gbm.model <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days + lock.perc + crush.perc + lead.perc, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.02, n.trees=1000, verbose=T, interaction.depth=2)
+gbm.model.new <- gbm(factor(winner.rank) ~ middle.diff.adj + middle.ratio + bottom.diff.adj + bottom.ratio + money.left.adj + dd.remaining + top.days + middle.days + bottom.days + lock.perc + crush.perc + lead.perc, data=modeling.points[!j.archive.id %in% test.ids], distribution="multinomial", shrinkage=0.02, n.trees=1000, verbose=T, interaction.depth=3)
 
 # Predict
-preds <- predict(gbm.model, modeling.points[j.archive.id %in% test.ids], n.trees=4000, type="response")[, , 1]
-preds.new <- predictGBM(gbm.model, modeling.points.new[j.archive.id %in% test.ids], n.trees=1500)
+preds <- predict(gbm.model, modeling.points[j.archive.id %in% test.ids], n.trees=700, type="response")[, , 1]
+preds.new <- predict(gbm.model.new, modeling.points[j.archive.id %in% test.ids], n.trees=700, type="response")[, , 1]
 
 # Evaluate calibration
 source("scripts/build_models/evaluate_model.R")
 evaluateModel(modeling.points[j.archive.id %in% test.ids], preds, units=0.05)
-evaluateModel(modeling.points.new[j.archive.id %in% test.ids], preds.new, units=0.05)
+evaluateModel(modeling.points[j.archive.id %in% test.ids], preds.new, units=0.05)
 
 # Show largest positive example
 row <- which.max(preds[, 1] - preds.new[, 1])
-# row <- which(preds[, 1] - preds.new[, 1] > 0.035)
+# row <- which(preds[, 1] - preds.new[, 1] > 0.1)
 modeling.points[j.archive.id %in% test.ids][row, ]
 preds[row, ]
 preds.new[row, ]
